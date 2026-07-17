d8546af31c999013982abe65d82fc3f84e8aa9d5
jingcheng <jingcheng070@gmail.com>
2026-07-17 14:59:34 +0800

    Update new version resume cv v5.1

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -83,7 +83,7 @@ const Header = () => {
           initial={{ y: 30, opacity: 0 }}
           whileInView={{ y: 0, opacity: 1 }}
           transition={{ duration: 0.6, delay: 1.2 }}
-          href="/Chiew Jing Cheng CV v4.0.pdf"
+          href="/Chiew Jing Cheng CV v5.1.pdf"
           download
           className="px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 dark:text-black bg-white "
         >
